eeae14f55f2cee5677a3a9ec768a7c69f71bc0b4
acheesylion <acheesylion@gmail.com>
2025-03-31 22:44:39 +0100

    with charlotte data

---
+++ src/main/java/model/TimeSlot.java
@@ -0,0 +1,69 @@
+package model;
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+public class TimeSlot {
+
+    // Private fields (as indicated by '-' in UML)
+    private DayOfWeek day;
+    private LocalDate startDate;
+    private LocalTime startTime;
+    private LocalDate endDate;
+    private LocalTime endTime;
+
+    // Public fields as indicated by '+' in UML (or accessed via getters/setters)
+    public String courseCode;
+    public int activityId;
+    public TimeSlotStatus status;
+
+    // Constructor initializing all fields
+    public TimeSlot(DayOfWeek day, LocalDate startDate, LocalTime startTime,
+                    LocalDate endDate, LocalTime endTime, String courseCode,
+                    int activityId, TimeSlotStatus status) {
+
+        this.day = day;
+        this.startDate = startDate;
+        this.startTime = startTime;
+        this.endDate = endDate;
+        this.endTime = endTime;
+        this.courseCode = courseCode;
+        this.activityId = activityId;
+        this.status = status;
+    }
+
+    // Checks if the TimeSlot has the given course code
+    public boolean hasCourseCode(String courseCode) {
+        if (this.courseCode == null) {
+            return false;
+        }
+        return this.courseCode.equals(courseCode);
+    }
+
+    // Checks if the TimeSlot has the given activity ID
+    public boolean hasActivityId(int id) {
+        return this.activityId == id;
+    }
+
+    // Returns true if the status of the TimeSlot is CHOSEN
+    public boolean isChosen() {
+        return this.status == TimeSlotStatus.CHOSEN;
+    }
+
+    // Provides a string representation of the TimeSlot
+    @Override
+    public String toString() {
+        return "TimeSlot{" +
+                "day=" + day +
+                ", startDate=" + startDate +
+                ", startTime=" + startTime +
+                ", endDate=" + endDate +
+                ", endTime=" + endTime +
+                ", courseCode='" + courseCode + '\'' +
+                ", activityId=" + activityId +
+                ", status=" + status +
+                '}';
+    }
+
+
+}
